00e91cf14cd0fbe45ba973ccfc4b7aa790d1c136
Ahmad Tayasnh <engahmadtayansa@gmail.com>
2025-01-28 13:07:23 +0200

    add colors and fonts and icon to our My Designer

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+ final  Color kPrimaryColor = const Color(0xffFFF6ED);  
+ final  Color kSecondaryColor = const Color(0xffE6BEAE);  
+ final  Color kThirdColor = const Color(0xffB2967D);  
+ final  Color kBackgroundButtonColor = const Color(0xff524539);  
+ final  Color kTextPrimaryColor = const Color(0xffB2967D);  
+ final  Color kTextSecondaryColor = const Color(0xff524539);  
+}
